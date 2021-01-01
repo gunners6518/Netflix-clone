e4cd02a81641f862b7088f3447d84090c35b6938
gunners6518 <gunners6518@gmail.com>
2021-01-01 16:45:05 +0900

    RowコンポーネントのDOM部分を作成

--- src/compornents/Row.tsx
+++ src/compornents/Row.tsx
@@ -3,14 +3,22 @@ import React, { useState, useEffect } from "react";
 import { readConfigFile } from "typescript";
 import { requests } from "../request";
 import axios from "./../axios";
+import "./Row.scss";
+
+const base_url = "https://image.tmdb.org/t/p/original";
 
 type Props = {
   title: string;
   fetchUrl: string;
 };
 
+type Movie = {
+  name: string;
+  poster_path: string;
+};
+
 export const Row = ({ title, fetchUrl }: Props) => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   //urlが更新される度に
   useEffect(() => {
@@ -25,8 +33,19 @@ export const Row = ({ title, fetchUrl }: Props) => {
   console.log(movies);
 
   return (
-    <div>
+    <div className="Row">
       <h2>{title}</h2>
+      <div className="Row-posters">
+        {/* ポスターコンテンツ */}
+        {movies.map((movie, i) => (
+          <img
+            className="Row-poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+            key={i}
+          />
+        ))}
+      </div>
     </div>
   );
 };
