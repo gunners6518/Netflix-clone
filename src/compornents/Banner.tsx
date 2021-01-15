228c0d92f2044c83199be5f5e07c9d83ab85cb33
gunners6518 <gunners6518@gmail.com>
2021-01-15 18:15:50 +0900

    Bannerコンポーネント作成

--- src/compornents/Banner.tsx
+++ src/compornents/Banner.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import { request } from "https";
+import React, { useState, useEffect } from "react";
+import axios from "./../axios";
+import { requests } from "./../request";
 
 type Props = {
   className?: string;
 };
 
+type movieProps = [];
+
 export const Banner = () => {
+  const [movie, setMovie] = useState<movieProps>([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.feachNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
   return (
-    <div>
-      <p>Banner</p>
-    </div>
+    <header className="Banner">
+      <div className="Banner-contents">
+        {/* <h1>{movie?.title || movie?.name || movie?.orignal_name}</h1> */}
+      </div>
+    </header>
   );
 };
