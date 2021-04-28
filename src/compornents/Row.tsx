e4a2f7f483531397692db1ce8f47c7b5a38218a8
gunners6518 <gunners6518@gmail.com>
2021-04-28 15:32:21 +0900

    EX-実装後のサンプルコード

--- src/compornents/Row.tsx
+++ src/compornents/Row.tsx
@@ -45,6 +45,8 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     fetchData();
   }, [fetchUrl]);
 
+  console.log(movies);
+
   const opts: Options = {
     height: "390",
     width: "640",
@@ -58,7 +60,9 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      let trailerurl = await axios.get(`/movie/${movie.id}/videos?api_key=XXX`);
+      let trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+      );
       setTrailerUrl(trailerurl.data.results[0]?.key);
     }
     //   movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
