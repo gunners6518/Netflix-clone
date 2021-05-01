40eb867eb5d277a04998b732e90ab39d88831d6f
gunners6518 <gunners6518@gmail.com>
2021-05-01 16:25:12 +0900

    ROWコンポーネントにJSOnを渡した

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,11 @@ function App() {
       <Nav />
       <Banner />
       {/* <Row title="NETFLIX ORIGUINALS" fetchUrl={} isLargeRow /> */}
-      <Row title="Top Rated" requests={requests.NetflixOriginals} />
+      <Row title="Top Rated" requests={requests.TopRate} />
       <Row title="Anime Movies" requests={requests.AnimeMovies} />
+      <Row title="Horror Movies" requests={requests.HorrorMovies} />
+      <Row title="Comdedy Movies" requests={requests.ComdedyMovies} />
+      <Row title="Documentary Movies" requests={requests.DocumentaryMovies} />
       <Row title="Romance Movies" requests={requests.RomanceMovies} />
     </div>
   );
