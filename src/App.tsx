812d12c8462cb400141c833704574ab9165170e0
gunners6518 <gunners6518@gmail.com>
2021-05-01 14:28:51 +0900

    RomanceMovies追加

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,7 @@ function App() {
       {/* <Row title="NETFLIX ORIGUINALS" fetchUrl={} isLargeRow /> */}
       <Row title="Top Rated" requests={requests.NetflixOriginals} />
       <Row title="Anime Movies" requests={requests.AnimeMovies} />
-      {/* <Row title="Action Movies"  />
-      <Row title="Comedy Movies"  />
-      <Row title="Horror Movies"  />
-      <Row title="Romance Movies"  />
-      <Row title="DOcumentaries" /> */}
+      <Row title="Romance Movies" requests={requests.RomanceMovies} />
     </div>
   );
 }
