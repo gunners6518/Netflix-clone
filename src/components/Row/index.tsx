8f24eb3dabc3fd0c3fcdb011dce37279a218bc33
kazuki.fujisawa <gunners6518@gmail.com>
2024-04-25 00:34:57 +0900

    add starter

--- src/components/Row/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useProps } from "./useProps";
-import { Layout, Props } from "./Layout";
-
-export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
-  return (
-    <Layout title={title} isLargeRow={isLargeRow} {...useProps(fetchUrl)} />
-  );
-};
